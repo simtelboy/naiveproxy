59a460100052a9a1ea4ad35c6ffb7f5074e65c41
hotyi <hotyi@DESKTOP-U2D8NGJ>
2026-01-07 23:37:53 +0800

    添加 track_event_internal.h 和 InternedData stub
    - 创建 perfetto/tracing/internal/track_event_internal.h
    - 定义 BaseTrackEventInternedDataIndex 和 TrackEventIncrementalState
    - 创建 InternedData pbzero stub
    - 修复 track_event_interned_data_index.h 的依赖

---
+++ src/third_party/perfetto/protos/perfetto/trace/interned_data/interned_data.pbzero.h
@@ -0,0 +1,24 @@
+// Copyright 2024 Stub implementation for OpenHarmony
+#ifndef PERFETTO_PROTOS_PERFETTO_TRACE_INTERNED_DATA_INTERNED_DATA_PBZERO_H_
+#define PERFETTO_PROTOS_PERFETTO_TRACE_INTERNED_DATA_INTERNED_DATA_PBZERO_H_
+
+namespace perfetto {
+namespace protos {
+namespace pbzero {
+
+// Stub for InternedData protobuf message
+class InternedData {
+ public:
+  InternedData() = default;
+  ~InternedData() = default;
+  
+  // Stub methods - do nothing
+  void set_iid(uint64_t) {}
+  void set_value(const char*) {}
+};
+
+}  // namespace pbzero
+}  // namespace protos
+}  // namespace perfetto
+
+#endif  // PERFETTO_PROTOS_PERFETTO_TRACE_INTERNED_DATA_INTERNED_DATA_PBZERO_H_
