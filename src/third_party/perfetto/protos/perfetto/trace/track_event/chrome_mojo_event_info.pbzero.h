93fd79743570d88589985601366b35c15f275733
hotyi <hotyi@DESKTOP-U2D8NGJ>
2026-01-08 21:14:35 +0800

    添加 chrome_mojo_event_info.pbzero.h stub 文件

---
+++ src/third_party/perfetto/protos/perfetto/trace/track_event/chrome_mojo_event_info.pbzero.h
@@ -0,0 +1,34 @@
+// Copyright 2025 Stub implementation for OpenHarmony
+// This is a stub file to satisfy build dependencies
+#ifndef THIRD_PARTY_PERFETTO_PROTOS_PERFETTO_TRACE_TRACK_EVENT_CHROME_MOJO_EVENT_INFO_PBZERO_H_
+#define THIRD_PARTY_PERFETTO_PROTOS_PERFETTO_TRACE_TRACK_EVENT_CHROME_MOJO_EVENT_INFO_PBZERO_H_
+
+#include <stdint.h>
+#include <string>
+
+namespace perfetto {
+namespace protos {
+namespace pbzero {
+
+// Stub implementation of ChromeMojoEventInfo
+class ChromeMojoEventInfo {
+ public:
+  ChromeMojoEventInfo() = default;
+  ~ChromeMojoEventInfo() = default;
+
+  // Stub methods
+  void set_mojo_interface_tag(const char* value) {}
+  const char* mojo_interface_tag() const { return ""; }
+
+  void set_ipc_hash(uint32_t value) {}
+  uint32_t ipc_hash() const { return 0; }
+
+  void set_is_reply(bool value) {}
+  bool is_reply() const { return false; }
+};
+
+}  // namespace pbzero
+}  // namespace protos
+}  // namespace perfetto
+
+#endif  // THIRD_PARTY_PERFETTO_PROTOS_PERFETTO_TRACE_TRACK_EVENT_CHROME_MOJO_EVENT_INFO_PBZERO_H_
