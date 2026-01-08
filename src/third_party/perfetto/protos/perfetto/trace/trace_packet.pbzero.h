4a3b2b51efbb3fb384f74cd440c2c6dc43b678bd
hotyi <hotyi@DESKTOP-U2D8NGJ>
2026-01-08 18:05:18 +0800

    添加 trace_packet.pbzero.h stub

---
+++ src/third_party/perfetto/protos/perfetto/trace/trace_packet.pbzero.h
@@ -0,0 +1,30 @@
+// Copyright 2025 Stub implementation for OpenHarmony
+// This is a stub file to satisfy build dependencies
+#ifndef THIRD_PARTY_PERFETTO_PROTOS_PERFETTO_TRACE_TRACE_PACKET_PBZERO_H_
+#define THIRD_PARTY_PERFETTO_PROTOS_PERFETTO_TRACE_TRACE_PACKET_PBZERO_H_
+
+#include <stdint.h>
+
+namespace perfetto {
+namespace protos {
+namespace pbzero {
+
+// Stub for TracePacket
+class TracePacket {
+ public:
+  TracePacket() = default;
+  ~TracePacket() = default;
+
+  // Stub methods
+  void set_timestamp(uint64_t value) {}
+  uint64_t timestamp() const { return 0; }
+
+  void set_timestamp_clock_id(uint32_t value) {}
+  uint32_t timestamp_clock_id() const { return 0; }
+};
+
+}  // namespace pbzero
+}  // namespace protos
+}  // namespace perfetto
+
+#endif  // THIRD_PARTY_PERFETTO_PROTOS_PERFETTO_TRACE_TRACE_PACKET_PBZERO_H_
