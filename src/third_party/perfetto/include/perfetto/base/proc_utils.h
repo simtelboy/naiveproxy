c9b9450603d01170e139b9cd5ea2a061675e6fe8
hotyi <hotyi@DESKTOP-U2D8NGJ>
2026-01-08 14:18:04 +0800

    修复 proc_utils.h 包含路径和添加 chrome_enums.pbzero.h stub

--- src/third_party/perfetto/include/perfetto/base/proc_utils.h
+++ src/third_party/perfetto/include/perfetto/base/proc_utils.h
@@ -19,7 +19,7 @@
 
 #include <stdint.h>
 
-#include "perfetto/base/build_config.h"
+#include "third_party/perfetto/include/perfetto/base/build_config.h"
 
 #if PERFETTO_BUILDFLAG(PERFETTO_OS_WIN)
 extern "C" {
